9e7ea5d69e318fb4e9d6d24ca61b775e10cfac4a
eddguy <ecubasmarine2019@fau.edu>
2023-12-04 15:57:53 -0500

    changes to login

--- timeclock.js
+++ timeclock.js
@@ -72,6 +72,7 @@ app.post('/login', async (req, res) => {
       const user = await Parse.User.logIn(userId, password);
       // Successfully logged in
       res.status(200).json({ message: 'Successful login', user: user.toJSON() });
+      const username = userId;
     } catch (error) {
       // Failed to log in (wrong username or password)
       res.status(401).json({ message: 'Wrong username or password' });
@@ -186,10 +187,227 @@ app.post('/logout', async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /users/{userId}:
+ *  get:
+ *    summary: Get user information
+ *    description: Use this to look at user information
+ *    parameters:
+ *      - name: userId
+ *        description: User's ID
+ *        in: path
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Successful 
+ *      404:
+ *        description: Error -- User not found
+ */
+app.get('/users/:userId', async (req, res) => {
+  var userId = req.params.userId; // Fetching userId from request parameters
+  const User = Parse.User;
+  const query = new Parse.Query(User);
 
-const port = process.env.PORT || 5678;
-app.listen(port, () => {
-  console.log('Server is running...');
-  console.log(`Webapp:   http://localhost:${port}/`);
-  console.log(`API Docs: http://localhost:${port}/api-docs`);
+  try {
+    let user = await query.get(userId);
+    console.log('User found', user);
+    res.status(200).json({ user: user.toJSON() }); // Sending the user data in the response
+  } catch (error) {
+    console.error('Error while fetching user', error);
+    res.status(404).json({ message: 'User not found' }); // Sending error response if user not found
+  }
 });
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Get all user information
+ *     description: Use this to retrieve all user information
+ *     responses:
+ *       200:
+ *         description: Successful retrieval of user information
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ *       404:
+ *         description: Error - Users not found
+ */
+// Assuming 'user' contains the array of user objects
+app.get('/users', async (req, res) => {
+  const Users = Parse.Object.extend('Users');
+  const query = new Parse.Query(Users);
+
+  try {
+    const results = await query.find(userId);
+    const usersData = results.map(object => ({
+      Username: object.get('username'),
+      First_name: object.get('first_name'),
+      Last_name: object.get('last_name'),
+      Email: object.get('email'),
+  
+    
+    }));
+  
+    res.json({ users: usersData }); // Sending schedules data back as JSON
+  } catch (error) {
+    console.error('Error while fetching Schedules', error);
+    res.status(500).send('Error fetching schedules'); // Sending an error status and message back
+  }
+});
+
+
+
+/**
+ * @swagger
+ * /classes/schedules:
+ *   post:
+ *     summary: Create schedules
+ *     description: Used to create schedules
+ *     parameters:
+ *       - name: Employee
+ *         description: Employee name
+ *         in: formData
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: Week
+ *         description: Work week
+ *         in: formData
+ *         required: true
+ *         format: date
+ *         schema:
+ *           type: string
+ *       - name: Sunday
+ *         description: Sunday schedule
+ *         in: formData
+ *         required: false
+ *         schema:
+ *           type: string
+ *       - name: Monday
+ *         description: Monday schedule
+ *         in: formData
+ *         required: false
+ *         schema:
+ *           type: string
+ *       - name: Tuesday
+ *         description: Tuesday schedule
+ *         in: formData
+ *         required: false
+ *         schema:
+ *           type: string
+ *       - name: Wednesday
+ *         description: Wednesday schedule
+ *         in: formData
+ *         required: false
+ *         schema:
+ *           type: string
+ *       - name: Thursday
+ *         description: Thursday schedule
+ *         in: formData
+ *         required: false
+ *         schema:
+ *           type: string
+ *       - name: Friday
+ *         description: Friday schedule
+ *         in: formData
+ *         required: false
+ *         schema:
+ *           type: string
+ *       - name: Saturday
+ *         description: Saturday schedule
+ *         in: formData
+ *         required: false
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Schedule created successfully
+ *       400:
+ *         description: Invalid input provided
+ */
+//Creating Schedules
+app.post('/classes/schedules', async (req, res) => {
+  const { userId, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Employee, Week } = req.body;
+  
+  const myNewObject = new Parse.Object('Schedules');
+  myNewObject.set('Employee', Employee);
+  myNewObject.set('Week', Week);
+  myNewObject.set('Sunday', Sunday);
+  myNewObject.set('Monday', Monday);
+  myNewObject.set('Tuesday', Tuesday);
+  myNewObject.set('Wednesday', Wednesday);
+  myNewObject.set('Thursday', Thursday);
+  myNewObject.set('Friday', Friday);
+  myNewObject.set('Saturday', Saturday);
+  
+  
+
+  try {
+    const result = await myNewObject.save();
+    console.log('Schedules created', result);
+    res.status(200).json({ message: 'Schedule created successfully', result });
+  } catch (error) {
+    console.error('Error while creating Schedules: ', error);
+    res.status(400).json({ message: 'Invalid input provided' });
+  }
+});
+
+/**
+ * @swagger
+ * /classes/schedules/{userId}:
+ *  get:
+ *    summary: Get user information
+ *    description: Use this to look at user information
+ *    parameters:
+ *       - name: Employee
+ *         description: Employee name
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *    responses:
+ *      200:
+ *        description: Successful 
+ *      404:
+ *        description: Error -- User not found
+ */
+//Reading schedules
+app.get('/classes/Schedules', async (req, res) => {
+  const Schedules = Parse.Object.extend('Schedules');
+  const query = new Parse.Query(Schedules);
+
+  try {
+    const results = await query.find(userId);
+    const schedulesData = results.map(object => ({
+      Employee: object.get('Employee'),
+      Week: object.get('Week'),
+      Sunday: object.get('Sunday'),
+      Monday: object.get('Monday'),
+      Tuesday: object.get('Tuesday'),
+      Wednesday: object.get('Wednesday'),
+      Thursday: object.get('Thursday'),
+      Friday: object.get('Friday'),
+      Saturday: object.get('Saturday'),
+    
+    }));
+  
+    res.json({ schedules: schedulesData }); // Sending schedules data back as JSON
+  } catch (error) {
+    console.error('Error while fetching Schedules', error);
+    res.status(500).send('Error fetching schedules'); // Sending an error status and message back
+  }
+});
+
+
+port = process.env.PORT || 5678;
+var listener = app.listen(port);
+  console.log('Server is running...');
+  console.log(`Webapp:   http://localhost:${port}/login.html`);
+  console.log(`API Docs: http://localhost:${port}/api-docs`);
